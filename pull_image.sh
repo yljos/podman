70afcf3c818febfbd5b073e24bb649d95d87498b
yljos <yljos@outlook.com>
2025-07-30 17:43:33 +0800

    add

---
+++ pull_image.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+# 1. 仅更新正在运行的容器镜像
+podman ps --format "{{.Image}} {{.Names}}" | while read image name; do
+    echo "拉取镜像: $image"
+    podman image pull "$image"
+    echo "重启容器: $name"
+    systemctl --user restart "$name"
+done
+
+# 2. 清理无用镜像
+echo "清理无用镜像..."
+podman image prune -af
+
+echo "批量更新完成。"
